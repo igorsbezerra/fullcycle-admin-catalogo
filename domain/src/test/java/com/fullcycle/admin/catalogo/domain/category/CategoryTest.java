2e4d4ca8197004d68933fceec71e33b99763537d
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-29 10:46:52 -0300

    test: validate category name cannot be null

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -23,4 +23,20 @@ class CategoryTest {
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
     }
+
+    @Test
+    void givenAnInvalidNullName_whenCallNewCategoryAnValidate_thenShouldReceivedError() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedActive = true;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate());
+        Assertions.assertEquals(expectedErrorCount , actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage , actualException.getErrors().get(0).getMessage());
+    }
 }
